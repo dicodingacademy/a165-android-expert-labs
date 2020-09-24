c347421543d5cb8632b8e648aef6c57f2295d92f
Arif <faizin.arief@gmail.com>
2020-09-24 11:52:32 +0700

    Fix: MasterKey in EncryptedSharedPreferences

--- MySimpleLogin/MySimpleLogin (encryption)/app/src/main/java/com/dicoding/mysimplelogin/SessionManager.kt
+++ MySimpleLogin/MySimpleLogin (encryption)/app/src/main/java/com/dicoding/mysimplelogin/SessionManager.kt
@@ -3,8 +3,10 @@ package com.dicoding.mysimplelogin
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Build
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
 import androidx.security.crypto.EncryptedSharedPreferences
-import androidx.security.crypto.MasterKeys
+import androidx.security.crypto.MasterKey
 import com.securepreferences.SecurePreferences
 
 class SessionManager(context: Context) {
@@ -14,11 +16,22 @@ class SessionManager(context: Context) {
     }
 
     private var pref: SharedPreferences = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+        val spec = KeyGenParameterSpec.Builder(
+            MasterKey.DEFAULT_MASTER_KEY_ALIAS,
+            KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+        )
+            .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
+            .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
+            .setKeySize(MasterKey.DEFAULT_AES_GCM_MASTER_KEY_SIZE)
+            .build()
+        val masterKey = MasterKey.Builder(context)
+            .setKeyGenParameterSpec(spec)
+            .build()
         EncryptedSharedPreferences
             .create(
-                "Session",
-                MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC),
                 context,
+                "Session",
+                masterKey,
                 EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
                 EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
             )
