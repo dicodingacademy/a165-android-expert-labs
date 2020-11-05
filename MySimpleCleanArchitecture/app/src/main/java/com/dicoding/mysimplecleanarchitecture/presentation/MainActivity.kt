3df8f8930ab7a0c831305ed5010d3405b8328918
Arif <faizin.arief@gmail.com>
2020-11-05 09:11:08 +0700

    Update Android Studio 4.1

--- MySimpleCleanArchitecture/app/src/main/java/com/dicoding/mysimplecleanarchitecture/presentation/MainActivity.kt
+++ MySimpleCleanArchitecture/app/src/main/java/com/dicoding/mysimplecleanarchitecture/presentation/MainActivity.kt
@@ -2,10 +2,10 @@ package com.dicoding.mysimplecleanarchitecture.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.dicoding.mysimplecleanarchitecture.R
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,12 +13,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val tvWelcome = findViewById<TextView>(R.id.tv_welcome)
+
         val factory = MainViewModelFactory.getInstance()
         val viewModel = ViewModelProvider(this, factory)[MainViewModel::class.java]
 
         viewModel.setName("Dicoding")
         viewModel.message.observe(this, Observer {
-            tv_welcome.text = it.welcomeMessage
+            tvWelcome.text = it.welcomeMessage
         })
     }
 }
