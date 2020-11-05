af8b178a277fd2bda1fbbe0dd2dde12351fca4aa
Arif <faizin.arief@gmail.com>
2020-11-05 16:00:34 +0700

    Update Android Studio 4.1

--- MySimpleLogin/MySimpleLogin (koin-solution)/app/src/main/java/com/dicoding/mysimplelogin/HomeActivity.kt
+++ MySimpleLogin/MySimpleLogin (koin-solution)/app/src/main/java/com/dicoding/mysimplelogin/HomeActivity.kt
@@ -3,24 +3,26 @@ package com.dicoding.mysimplelogin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_home.*
+import com.dicoding.mysimplelogin.databinding.ActivityHomeBinding
 import org.koin.android.ext.android.inject
 
 class HomeActivity : AppCompatActivity() {
 
-    val userRepository: UserRepository by inject()
+    lateinit var binding: ActivityHomeBinding
+    private val userRepository: UserRepository by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 //        hapus kode berikut
 //        val sesi = SessionManager(this)
 //        userRepository = UserRepository.getInstance(sesi)
 
-        tv_welcome.text = "Welcome ${userRepository.getUser()}"
+        binding.tvWelcome.text = "Welcome ${userRepository.getUser()}"
 
-        btn_logout.setOnClickListener {
+        binding.btnLogout.setOnClickListener {
             userRepository.logoutUser()
             moveToMainActivity()
         }
--- MySimpleLogin/MySimpleLogin (koin-solution)/app/src/main/java/com/dicoding/mysimplelogin/MainActivity.kt
+++ MySimpleLogin/MySimpleLogin (koin-solution)/app/src/main/java/com/dicoding/mysimplelogin/MainActivity.kt
@@ -3,16 +3,18 @@ package com.dicoding.mysimplelogin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import com.dicoding.mysimplelogin.databinding.ActivityMainBinding
 import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
 
-    val userRepository: UserRepository by inject()
+    lateinit var binding: ActivityMainBinding
+    private val userRepository: UserRepository by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 //        hapus kode berikut
 //        val sesi = SessionManager(this)
@@ -22,13 +24,13 @@ class MainActivity : AppCompatActivity() {
             moveToHomeActivity()
         }
 
-        btn_login.setOnClickListener {
+        binding.btnLogin.setOnClickListener {
             saveSession()
         }
     }
 
     private fun saveSession() {
-        userRepository.loginUser(ed_username.text.toString())
+        userRepository.loginUser(binding.edUsername.text.toString())
         moveToHomeActivity()
     }
 
