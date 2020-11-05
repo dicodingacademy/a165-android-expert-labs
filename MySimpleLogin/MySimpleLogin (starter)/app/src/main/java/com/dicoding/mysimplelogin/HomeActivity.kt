5ccd8d4fca4a9d1be884bca6a846540412d6ed0d
Arif <faizin.arief@gmail.com>
2020-11-05 14:57:58 +0700

    Update Android Studio 4.1

--- MySimpleLogin/MySimpleLogin (starter)/app/src/main/java/com/dicoding/mysimplelogin/HomeActivity.kt
+++ MySimpleLogin/MySimpleLogin (starter)/app/src/main/java/com/dicoding/mysimplelogin/HomeActivity.kt
@@ -1,24 +1,26 @@
 package com.dicoding.mysimplelogin
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_home.*
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.mysimplelogin.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityHomeBinding
     lateinit var userRepository: UserRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val sesi = SessionManager(this)
         userRepository = UserRepository.getInstance(sesi)
 
-        tv_welcome.text = "Welcome ${userRepository.getUser()}"
+        binding.tvWelcome.text = "Welcome ${userRepository.getUser()}"
 
-        btn_logout.setOnClickListener {
+        binding.btnLogout.setOnClickListener {
             userRepository.logoutUser()
             moveToMainActivity()
         }
--- MySimpleLogin/MySimpleLogin (starter)/app/src/main/java/com/dicoding/mysimplelogin/MainActivity.kt
+++ MySimpleLogin/MySimpleLogin (starter)/app/src/main/java/com/dicoding/mysimplelogin/MainActivity.kt
@@ -3,15 +3,17 @@ package com.dicoding.mysimplelogin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import com.dicoding.mysimplelogin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
     lateinit var userRepository: UserRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val sesi = SessionManager(this)
         userRepository = UserRepository.getInstance(sesi)
@@ -20,13 +22,13 @@ class MainActivity : AppCompatActivity() {
             moveToHomeActivity()
         }
 
-        btn_login.setOnClickListener {
+        binding.btnLogin.setOnClickListener {
             saveSession()
         }
     }
 
     private fun saveSession() {
-        userRepository.loginUser(ed_username.text.toString())
+        userRepository.loginUser(binding.edUsername.text.toString())
         moveToHomeActivity()
     }
 
