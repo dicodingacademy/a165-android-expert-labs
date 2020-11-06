e16d912b21741df2ae35948aae50024cba6e8fd0
Arif <faizin.arief@gmail.com>
2020-11-06 15:05:23 +0700

    Update Android Studio 4.1

--- MySimpleLogin/MySimpleLogin (encryption)/app/src/main/java/com/dicoding/mysimplelogin/HomeActivity.kt
+++ MySimpleLogin/MySimpleLogin (encryption)/app/src/main/java/com/dicoding/mysimplelogin/HomeActivity.kt
@@ -3,22 +3,24 @@ package com.dicoding.mysimplelogin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_home.*
+import com.dicoding.mysimplelogin.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityHomeBinding
     lateinit var userRepository: UserRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val sesi = SessionManager(this)
         userRepository = UserRepository.getInstance(sesi)
 
-        tv_welcome.text = "Welcome ${userRepository.getUser()}"
+        binding.tvWelcome.text = "Welcome ${userRepository.getUser()}"
 
-        btn_logout.setOnClickListener {
+        binding.btnLogout.setOnClickListener {
             userRepository.logoutUser()
             moveToMainActivity()
         }
