3dae01a58fa3e71659ae0f01cc2bab39e7b4a373
Arif <faizin.arief@gmail.com>
2020-11-06 15:54:31 +0700

    Update Android Studio 4.1

--- MyReportApp/MyReportApp (Firebase Crashlytics)/app/src/main/java/com/dicoding/myreportapp/MainActivity.kt
+++ MyReportApp/MyReportApp (Firebase Crashlytics)/app/src/main/java/com/dicoding/myreportapp/MainActivity.kt
@@ -1,16 +1,17 @@
 package com.dicoding.myreportapp
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.crashlytics.FirebaseCrashlytics
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_crash.setOnClickListener {
+        val btnCrash = findViewById<Button>(R.id.btn_crash)
+        btnCrash.setOnClickListener {
             FirebaseCrashlytics.getInstance().log("Clicked on button")
             FirebaseCrashlytics.getInstance().setCustomKey("str_key", "some_data")
             throw RuntimeException("Test Crash")
