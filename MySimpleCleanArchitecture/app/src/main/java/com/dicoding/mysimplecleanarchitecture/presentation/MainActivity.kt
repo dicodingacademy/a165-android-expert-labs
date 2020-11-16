7a0b10a3bd02ed3378b2dc7f6bd5a2c72dbfd82f
Arif <faizin.arief@gmail.com>
2020-11-16 12:53:54 +0700

    Implement ViewBinding

--- MySimpleCleanArchitecture/app/src/main/java/com/dicoding/mysimplecleanarchitecture/presentation/MainActivity.kt
+++ MySimpleCleanArchitecture/app/src/main/java/com/dicoding/mysimplecleanarchitecture/presentation/MainActivity.kt
@@ -6,21 +6,24 @@ import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.dicoding.mysimplecleanarchitecture.R
+import com.dicoding.mysimplecleanarchitecture.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val tvWelcome = findViewById<TextView>(R.id.tv_welcome)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val factory = MainViewModelFactory.getInstance()
         val viewModel = ViewModelProvider(this, factory)[MainViewModel::class.java]
 
         viewModel.setName("Dicoding")
         viewModel.message.observe(this, Observer {
-            tvWelcome.text = it.welcomeMessage
+            binding.tvWelcome.text = it.welcomeMessage
         })
     }
 }
